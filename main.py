bc41b23a64372e92689c200293ad78dc6417683d
Jackson Thompson <thompsonjackson2004@gmail.com>
2026-02-28 21:26:06 -0800

    Added documentation file and main now outputs to a output.txt file in same directory

--- main.py
+++ main.py
@@ -7,19 +7,31 @@ Class: CPSC 323-07
 Authors: Braedon Collett, Jackson Thompson, and Tony Lin
 Description:
 """
+import sys
 from lexer import tokenize
 def main():
-    #Example 1: 
-    print("Example 1")
-    for _ in tokenize("while (fahr <= upper) a = 23.00;"):
-        print(_)
-    print("Example 2")
-    for _ in tokenize("if (fahr <= upper) a = 23.00;"):
-        print(_)
-    print("Example 3")
-    for _ in tokenize("var_1 = 1"):
-        print(_)
-    pass
+    with open("output.txt", "w") as f:
+        class Tee:
+            def write(self, data):
+                sys.__stdout__.write(data)
+                f.write(data)
+            def flush(self):
+                sys.__stdout__.flush()
+                f.flush()
+
+        sys.stdout = Tee()
+
+        #Example 1: 
+        print("Example 1")
+        for _ in tokenize("while (fahr <= upper) a = 23.00;"):
+            print(_)
+        print("Example 2")
+        for _ in tokenize("if (fahr <= upper) a = 23.00;"):
+            print(_)
+        print("Example 3")
+        for _ in tokenize("var_1 = 1"):
+            print(_)
+        pass
 
 if __name__ == '__main__':
     main()
